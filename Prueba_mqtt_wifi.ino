ad1d221fb9af38f704896d031d8b8a9e1cf26d89
Juan Pérez <133648979+JuanPerez07@users.noreply.github.com>
2024-05-30 11:38:29 +0200

    commented Prueba_mqtt_wifi.ino

--- Prueba_mqtt_wifi.ino
+++ Prueba_mqtt_wifi.ino
@@ -1,33 +1,25 @@
 #include "config.h" 
-// configuración pantalla OLED
-const int oledAddress = 0x3C;  // La dirección I2C de la pantalla OLED
-const int sdaPin = 5;  // Pin SDA, puede variar según tu ESP32
-const int sclPin = 4;  // Pin SCL, puede variar según tu ESP32
-// Crea un objeto display
+// OLED screen config
+const int oledAddress = 0x3C;  // I2C direction of OLED screen
+const int sdaPin = 5;
+const int sclPin = 4; 
+// display object
 SSD1306Wire display(oledAddress, sdaPin, sclPin);
-// Variables de estado de conexión
+// State of connection variables
 bool internetOk = false;
 bool brokerOk = false;
 bool sensorOk = false;
-// Configuración de username y password del WiFi
-//    const char* ssid;
-//    const char* password;
-// Configuración de la dirección y puerto del broker
-//    const char* server_mqtt;
-//    const int puerto_mqtt;
-// configuración de username y password del cliente 
-//    const char* mqtt_username;
-//    const char* mqtt_password;
-// Configuración del certificado de raíz: token necesario para la seguridad TLS
-//    static const char* root_ca;
+// Declaration of the client objects
 WiFiClientSecure ClientEsp;
 PubSubClient client(ClientEsp);
+// global variables read by the sensor DHT11
 static float temp = 0;
 static float hum = 0;
+// data used in the callback 
 String _topic;
 String _payload;
 // sensor GPIO signal and type of sensor 
-#define PIN_SIGNAL 25  
+const int PIN_SIGNAL = 25;  
 DHT dht(PIN_SIGNAL, DHT11);
 // function to read values from the sensor each 5 seconds
 void readSensor(){
@@ -50,13 +42,13 @@ void wificonf(){
   internetOk = true;
   delay(500);
 }
-// function to connect to the HiveMQ borker
+// function to connect to the HiveMQ broker
 void connectMQTT(){
   while (!client.connected()){
     if (client.connect("ESP32Client", mqtt_username, mqtt_password)){
       brokerOk = true;
     } else{ // connection failure
-      Serial.print("Falla, estado= ");
+      Serial.print("Failure, state = ");
       Serial.println(client.state());
       delay(5000); 
     }
@@ -114,16 +106,10 @@ void loop(){
     display.drawString(display.getWidth() / 2, 31, "DHT11 connection lost");
   }
   display.display();  // show messages in the screen OLED
-  //--------------------------------------------------------------
+  //--------------------------------------------------------------------------
   // publish to the broker in the cloud
   if(not isnan(hum) && not isnan(temp)){
     client.publish("temperature", String(temp).c_str());
     client.publish("humidity", String(hum).c_str());
   }
 }
-
-
-
-
-
-
